8e65fdfd5f5a4e7927b26b9e21c433b22ee3b2b2
damian.kaplon <kaplon.damian99@gmail.com>
2022-06-13 10:09:58 +0200

    blas gemm on CPU with user input

--- src/Matrix_mul_mkl.cpp
+++ src/Matrix_mul_mkl.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <limits>
 #include <chrono>
+
 #include <CL/sycl.hpp>
 #include "oneapi/mkl.hpp"
 #include <iostream>
@@ -9,13 +10,17 @@ using namespace std::chrono;
 using namespace std;
 
 int main() {
+    int a;
+
+    cout << "Podaj rozmiar: "; 
+    cin >> a;
 
     auto transA = oneapi::mkl::transpose::nontrans;
     auto transB = oneapi::mkl::transpose::nontrans;
 
-    int m = 100;  // A = m*k
-    int k = 100;  // B = k*n
-    int n = 100;  // C = m*n
+    int m = a;  // A = m*k
+    int k = a;  // B = k*n
+    int n = a;  // C = m*n
 
     int lda = k;
     int ldb = n;
@@ -24,75 +29,40 @@ int main() {
     double alpha = 1.0;
     double beta = 0.0;
 
-    sycl::queue cpu_device_queue{sycl::cpu_selector{}};
-    sycl::queue gpu_device_queue{sycl::gpu_selector{}};
+    sycl::queue device_queue{sycl::cpu_selector{}};
 
-    auto A_CPU = sycl::malloc_shared<double>(m * k, cpu_device_queue);
-    auto B_CPU = sycl::malloc_shared<double>(k * n, cpu_device_queue);
-    auto C_CPU = sycl::malloc_shared<double>(m * n, cpu_device_queue);
 
-    auto A_GPU = sycl::malloc_shared<double>(m * k, gpu_device_queue);
-    auto B_GPU = sycl::malloc_shared<double>(k * n, gpu_device_queue);
-    auto C_GPU = sycl::malloc_shared<double>(m * n, gpu_device_queue);
+    auto A = sycl::malloc_shared<double>(m * k, device_queue);
+    auto B = sycl::malloc_shared<double>(k * n, device_queue);
+    auto C = sycl::malloc_shared<double>(m * n, device_queue);
 
-    if (!A_CPU || !B_CPU || !C_CPU || !A_GPU || !B_GPU || !C_GPU) {
-        std::cerr << "Could not allocate memory for matrices." << std::endl;
+    if (!A || !B || !C) {
+        cout << "Could not allocate memory for matrices." << endl;
         exit(1);
     }
 
         // Initialize matrix data.
     for (int i = 0; i < m; i++)
-        for (int j = 0; j < k; j++) {
-            A_CPU[i * lda + j] = 1;
-            A_GPU[i * lda + j] = 1;        // tu wartość 1
-        }
+        for (int j = 0; j < k; j++)
+            A[i * lda + j] = rand() %100;       
+ 
 
     for (int i = 0; i < k; i++)
-        for (int j = 0; j < n; j++) {
-            B_CPU[i * ldb + j] = 1;
-            B_GPU[i * ldb + j] = 1;        // tu wartość 2
-        }
+        for (int j = 0; j < n; j++)
+            B[i * ldb + j] = rand() %100;       
 
 
-    std::cerr << "Launching oneMKL CPU GEMM calculations..." << std::endl;
-    auto cpu_start = high_resolution_clock::now();
-    oneapi::mkl::blas::row_major::gemm(cpu_device_queue, transA, transB, m, n, k,
-                                           alpha, A_CPU, lda, B_CPU, ldb, beta, C_CPU, ldc);
-
-        // While calculation occurs, compute reference result to check accuracy.
-    //    std::cerr << "Performing reference calculation..." << std::endl;
-  //  for (int i = 0; i < m; i++)
-      //  for (int h = 0; h < k; h++)
-          //  for (int j = 0; j < n; j++)
-             //   C_reference[i * ldc + j] += A[i * lda + h] * B[h * ldb + j];
-        
-    cpu_device_queue.wait_and_throw();
-    auto cpu_end = high_resolution_clock::now();
-    auto cpu_duration = duration_cast<microseconds>(cpu_end - cpu_start);
-    cout <<"CPU TIME: "<<cpu_duration.count() << std::endl;
-    free(C_CPU, cpu_device_queue);
-    free(A_CPU, cpu_device_queue);
-    free(B_CPU, cpu_device_queue);
-
-    std::cerr << "Launching oneMKL GPU GEMM calculations..." << std::endl;
-    auto gpu_start = high_resolution_clock::now();
-    oneapi::mkl::blas::row_major::gemm(gpu_device_queue, transA, transB, m, n, k,
-                                           alpha, A_GPU, lda, B_GPU, ldb, beta, C_GPU, ldc);
-
-        // While calculation occurs, compute reference result to check accuracy.
-    //    std::cerr << "Performing reference calculation..." << std::endl;
-  //  for (int i = 0; i < m; i++)
-      //  for (int h = 0; h < k; h++)
-          //  for (int j = 0; j < n; j++)
-             //   C_reference[i * ldc + j] += A[i * lda + h] * B[h * ldb + j];
-        
-    gpu_device_queue.wait_and_throw();
-    auto gpu_end = high_resolution_clock::now();
-    auto gpu_duration = duration_cast<microseconds>(gpu_end - gpu_start);
-    cout <<"GPU TIME: "<<gpu_duration.count() << std::endl;
-    free(C_GPU, gpu_device_queue);
-    free(A_GPU, gpu_device_queue);
-    free(B_GPU, gpu_device_queue);
+    auto start = high_resolution_clock::now();
+    cout << "Launching oneMKL GEMM calculation..." << endl;
+    oneapi::mkl::blas::row_major::gemm(device_queue, transA, transB, m, n, k,
+                                           alpha, A, lda, B, ldb, beta, C, ldc);
+    device_queue.wait_and_throw();
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end - start);
+    cout << "TIME: " << duration.count() << "ms" << endl;
+    free(A, device_queue);
+    free(B, device_queue);
+    free(C, device_queue);
 
     return 0;
 }
